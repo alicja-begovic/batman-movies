d4620df442787453dd6ceeebd4dba02693482649
Alicja Begović <alicja.begovic@gmail.com>
2024-05-13 22:25:13 +0200

    preparing code and readme cover

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+//import reportWebVitals from "./reportWebVitals";
+//import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -14,13 +15,16 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDg690NXmhqpO3AnXhuR_cyPBaFa1GdWjs",
-  authDomain: "test-de3f5.firebaseapp.com",
-  projectId: "test-de3f5",
-  storageBucket: "test-de3f5.appspot.com",
-  messagingSenderId: "526001346743",
-  appId: "1:526001346743:web:7b9719bd470e7d2b5bd532"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDg690NXmhqpO3AnXhuR_cyPBaFa1GdWjs",
+//   authDomain: "test-de3f5.firebaseapp.com",
+//   projectId: "test-de3f5",
+//   storageBucket: "test-de3f5.appspot.com",
+//   messagingSenderId: "526001346743",
+//   appId: "1:526001346743:web:7b9719bd470e7d2b5bd532",
+// };
+
+// //init firebase app
+// initializeApp(firebaseConfig);
